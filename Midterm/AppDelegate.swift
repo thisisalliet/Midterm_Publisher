97a10ba12ad21c93bca60c8a79162a738336f5c4
Allie <theallietang@gmail.com>
2021-10-11 19:00:04 +0800

    Feat: install Firebase

--- Midterm/AppDelegate.swift
+++ Midterm/AppDelegate.swift
@@ -6,14 +6,15 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseFirestore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
--- Midterm/ViewController.swift
+++ Midterm/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseFirestoreSwift
 
 class ViewController: UIViewController {
 
