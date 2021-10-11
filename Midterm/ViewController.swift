904698b05079696a3e440acc1ab37cf28b7f97d4
Allie <theallietang@gmail.com>
2021-10-11 18:22:43 +0800

    Initial Commit

---
+++ Midterm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Midterm
+//
+//  Created by Allie T on 2021/10/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
