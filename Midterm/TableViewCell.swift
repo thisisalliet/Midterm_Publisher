980b92c03a8eaa5dc3e31cf1d45bd51ec5af24f1
Allie <theallietang@gmail.com>
2021-10-12 09:20:30 +0800

    Feat: add tableview to launch page

---
+++ Midterm/TableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  TableViewCell.swift
+//  Midterm
+//
+//  Created by Allie T on 2021/10/12.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var authorLabel: UILabel!
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    @IBOutlet weak var contentLabel: UILabel!
+    
+    @IBOutlet weak var categoryLabel: UILabel!
+
+}
