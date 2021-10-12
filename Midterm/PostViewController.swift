72037598857c4dd64369945743a2961add530d98
Allie <theallietang@gmail.com>
2021-10-12 09:36:58 +0800

    Feat: create post page and add tabBar

---
+++ Midterm/PostViewController.swift
@@ -0,0 +1,37 @@
+//
+//  PostViewController.swift
+//  Midterm
+//
+//  Created by Allie T on 2021/10/12.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var categoryLabel: UILabel!
+    
+    @IBOutlet weak var contentTextView: UITextView!
+    
+    @IBOutlet weak var postButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
